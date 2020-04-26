b4a0fbb2c9059083eb24f3118e94a2e82a3d4867
rajzz9 <rajavardhan.merugu@gmail.com>
2020-04-26 14:02:00 +0530

    spring boot and cloud example1

---
+++ Example-1/config-server/src/test/java/com/spring/vardhan/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vardhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Example-1/department-service/src/main/java/com/spring/vardhan/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package com.spring.vardhan.departmentservice.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.spring.vardhan.departmentservice.model.Employee;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+	@GetMapping("/department/{departmentId}")
+	List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+	
+}
